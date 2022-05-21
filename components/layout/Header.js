316040dcf636c1e4a48310ba8cf9ffc9d0b937d1
nglikwai <nglikwai@ymail.com>
2022-05-21 18:20:09 +0800

    upload

---
+++ components/layout/Header.js
@@ -0,0 +1,133 @@
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { loadUser } from "../../redux/actions/userActions";
+import { signOut } from "next-auth/client";
+
+const Header = () => {
+  const dispatch = useDispatch();
+
+  const { user, loading } = useSelector((state) => state.loadedUser);
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(loadUser());
+    }
+  }, [dispatch, user]);
+
+  const logoutHandler = () => {
+    signOut();
+  };
+
+  return (
+    <nav className="navbar row justify-content-center sticky-top">
+      <div className="container">
+        <div className="col-3 p-0">
+          <div className="navbar-brand">
+            <Link href="/" >
+              <h4>
+                <strong className="text-danger" style={{cursor:'pointer'}}>ONE BOOK</strong>
+              </h4>
+            </Link>
+          </div>
+        </div>
+        <Link href="/search">
+                <SearchButton>
+                  <FontAwesomeIcon
+                    icon={faMagnifyingGlass}
+                    color="white"
+                    size="lg"
+                  />
+                </SearchButton>
+              </Link>
+        <div className="col-3 mt-3 mt-md-0 text-center">
+          {user ? (
+            <div className="ml-4 dropdown d-line">
+              <a
+                className="btn dropdown-toggle mr-4"
+                id="dropDownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <figure className="avatar avatar-nav">
+                  <img
+                    src={user.avatar && user.avatar.url}
+                    alt={user && user.name}
+                    className="rounded-circle"
+                  />
+                </figure>
+                <span>{user && user.name}</span>
+              </a>
+  
+
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropDownMenuButton"
+              >
+                {user.role === "admin" && (
+                  <>
+                    <Link href="/admin/rooms">
+                      <a className="dropdown-item">Rooms</a>
+                    </Link>
+
+                    <Link href="/admin/bookings">
+                      <a className="dropdown-item">Bookings</a>
+                    </Link>
+
+                    <Link href="/admin/users">
+                      <a className="dropdown-item">Users</a>
+                    </Link>
+
+                    <Link href="/admin/reviews">
+                      <a className="dropdown-item">Reviews</a>
+                    </Link>
+
+                    <hr />
+                  </>
+                )}
+
+                <Link href="/bookings/me">
+                  <a className="dropdown-item">My Bookings</a>
+                </Link>
+
+                <Link href="/me/update">
+                  <a className="dropdown-item">Profile</a>
+                </Link>
+
+                <Link href="/">
+                  <a
+                    className="dropdown-item text-danger"
+                    onClick={logoutHandler}
+                  >
+                    Logout
+                  </a>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link href="/login">
+                <a className="px-4 py-2 text-secondary bg float-right border rounded-pill">
+                  Login
+                </a>
+              </Link>
+            )
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const SearchButton = styled.button`
+  background-color: #cc0000;
+  border-radius: 3rem;
+  border: none;
+  padding: 8px 12px;
+`;
+
+export default Header;
---
+++ next.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    DB_URI:
+      "mongodb+srv://nglikwai:dse00com@cluster0.evmdw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    STRIPE_API_KEY: 'pk_test_51Knoj4H2OfjmfYRL3e8qXlXcfs8QxZr8YWgzeoojC9Cllqp8A0smTqalOAB5Q5v93VGvscObHqxFXITzok4okL9h00VTLNbdFs',
+    STRIPE_SECRET_KEY: 'sk_test_51Knoj4H2OfjmfYRLczGv1hH5NU6TUx4iAtCZmfOrUvdI2ikGzCqPi9fIYmg25X3jPGFr25RIvqn9jfHG0dRFxyWf00DKJmQ6mG',
+    STRIPE_WEBHOOK_SECRET: 'we_1KwUlaH2OfjmfYRLUpYmXuUx',
+    CLOUDINARY_CLOUD_NAME: "nglikwai",
+    CLOUDINARY_API_KEY: "394655583168552",
+    CLOUDINARY_API_SECRET: "vFnjEXnhpBd8crXZWQDJoew5tCg",
+
+    SMTP_HOST: "smtp.mailtrap.io",
+    SMTP_PORT: "2525",
+    SMTP_USER: "e9daa884de3638",
+    SMTP_PASSWORD: "3d1f03db5eee80",
+    SMTP_FROM_NAME: "BOOKIT",
+    SMTP_FROM_EMAIL: "noreply@bookit.com",
+    NEXTAUTH_URL: "https://bookit-dse00.vercel.app"
+  },
+  images: {
+    domains: ["res.cloudinary.com","pix8.agoda.net"],
+  },
+};
+
